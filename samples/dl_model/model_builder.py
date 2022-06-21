2990de437fd9c36a9cdd424edafe156ac5bf63a7
park-tsp <chansoo0710@gmail.com>
2022-06-21 11:57:58 +0900

    Update: deep learning model

---
+++ samples/dl_model/model_builder.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+import tensorflow.keras.models as models
+from .model_zoo.UNet import unet
+from .model_zoo.DeepLabV3plus import DeeplabV3_plus
+from .model_zoo.modify_DeepLabV3plus import deepLabV3Plus
+from .model_zoo.EfficientNetV2 import EfficientNetV2S
+from .model_zoo.DDRNet_23_slim import ddrnet_23_slim
+from .model_zoo.mobileNetV3 import MobileNetV3_Small
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+
+def classifier(x, num_classes=19, upper=4, name=None):
+    x = tf.keras.layers.Conv2D(num_classes, 1, strides=1,
+                      kernel_initializer=tf.keras.initializers.VarianceScaling(scale=1.0, mode="fan_out", distribution="truncated_normal"))(x)
+    x = tf.keras.layers.UpSampling2D(size=(upper, upper), interpolation='bilinear', name=name)(x)
+    return x
+
+def segmentation_model(image_size):
+    # model_input, model_output = unet(input_shape=(image_size[0], image_size[1], 3), use_logits=False)
+    # return tf.keras.Model(model_input, model_output)
+    return ddrnet_23_slim(input_shape=(image_size[0], image_size[1], 3), num_classes=1)
+
+    
+
+def semantic_model(image_size, model='MobileNetV3S'):
+    if model=='MobileNetV3S':
+        base = MobileNetV3_Small(shape=(image_size[0], image_size[1], 3), n_class=1000, alpha=1, include_top=False).build()
+        # 224 1
+        # 112 2
+        # 56 4 -> add
+        # 28 8
+        # 14 16 -> add_5
+        # get output stride 
+            # 1/4 : re_lu_1
+            # 1/16
+        c5 = base.get_layer('add_5').output  # 16x32 256 or get_layer('post_swish') => 확장된 채널 1280
+        c2 = base.get_layer('add').output  # 128x256 48
+    else:
+        base = EfficientNetV2S(input_shape=(image_size[0], image_size[1], 3), pretrained="imagenet")
+        c5 = base.get_layer('add_34').output
+        c2 = base.get_layer('add_4').output 
+
+
+
+
+    """
+    for EfficientNetV2S (input resolution: 512x1024)
+    32x64 = 'add_34'
+    64x128 = 'add_7'
+    128x256 = 'add_4'
+    """
+    features = [c2, c5]
+
+    model_input = base.input
+    model_output = deepLabV3Plus(features=features, activation='swish')
+
+    semantic_output = classifier(model_output, num_classes=2, upper=4, name='output')
+
+    model = models.Model(inputs=[model_input], outputs=[semantic_output])
+    
+    return model
+
+
+class SemanticModel():
+    def __init__(self):
+        self.image_size = (224, 224)
+        self.weights = './dl_model/test_weight.h5'
+        self.load_model()
+        self.warm_up()
+    
+    def load_model(self):
+        base = MobileNetV3_Small(shape=(self.image_size[0], self.image_size[1], 3), n_class=1000, alpha=1, include_top=False).build()
+        
+        c5 = base.get_layer('add_5').output  # 16x32 256 or get_layer('post_swish') => 확장된 채널 1280
+        c2 = base.get_layer('add').output  # 128x256 48
+
+        features = [c2, c5]
+
+        model_input = base.input
+        model_output = deepLabV3Plus(features=features, activation='swish')
+
+        semantic_output = classifier(model_output, num_classes=2, upper=4, name='output')
+        semantic_output = tf.argmax(semantic_output, axis=-1)
+        self.model = models.Model(inputs=[model_input], outputs=[semantic_output])
+        return self.model
+
+    def warm_up(self):
+        self.model.load_weights(self.weights, by_name=True)
+        dummy_data = tf.zeros((1, self.image_size[0], self.image_size[1],3))
+        _ = self.model.predict(dummy_data)
+        print("warm up clear")
+
+    
+    def model_predict(self, image):
+        
+        image = tf.expand_dims(image, axis=0)
+        
+        image = tf.image.resize(image, size=(self.image_size[0], self.image_size[1]),
+            method=tf.image.ResizeMethod.BILINEAR)
+        
+        image = tf.cast(image, tf.float32)
+        image = preprocess_input(image, mode='torch')
+        
+        return self.model.predict(image)
+        
+    
+        
--- samples/web_socket_multi_test.py
+++ samples/web_socket_multi_test.py
@@ -1,50 +1,42 @@
 import ssl
 import asyncio
 import websockets
-
 import cv2
 import numpy as np
 import base64
-
 from threading import Thread
-from concurrent.futures import ProcessPoolExecutor
+from dl_model.model_builder import SemanticModel
 
-class TCPServer(object):
+
+class TCPServer(SemanticModel):
     def __init__(self, hostname, port, cert_dir, key_dir):
+        super().__init__()
         self.hostname = hostname
         self.port = port
         self.cert_dir = cert_dir
         self.key_dir = key_dir
-        self.configuration()
-
-
 
     def rcv_data(self, data):
-        data = data[0]
-        data = data.split(',')
+        data = data[0].split(',')
         client_id = data[0]
-        print('client id', client_id)
         base64_data = data[2]
-        # print('base64_data', base64_data)
-        
 
         if len(base64_data) % 4:
             # 4의 배수가 아니면 패딩
-            base64_data += '=' * (4 - len(base64_data) % 4) 
-            
+            base64_data += '=' * (4 - len(base64_data) % 4)
+
         imgdata = base64.b64decode(base64_data)
         image = np.frombuffer(imgdata, np.uint8)
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-
-        cv2.imshow(str(client_id), image)
-        # print(image.shape)
+        predict = self.model_predict(image=image)
+        predict = predict[0]
+        cv2.imshow(str(client_id), predict.astype(np.uint8)*255)
         cv2.waitKey(1)
 
-        # encoding
         encode_image = base64.b64encode(image)
         return client_id, encode_image
-
 
     async def loop_logic(self, websocket, path):
         while True:
@@ -52,28 +44,26 @@ class TCPServer(object):
             try:
                 # Wait data from client
                 data = await asyncio.gather(websocket.recv())
-                # data = await websocket.recv()
                 client_id, rcv_data = self.rcv_data(data=data)
 
-                await websocket.send(rcv_data);# 클라인언트로 echo를 붙여서 재 전송한다.
+                await websocket.send(rcv_data)
 
             except Exception as e:
-                
                 print('websocket client is disconnected!!  :', e)
                 cv2.destroyWindow(str(client_id))
 
-
-    def configuration(self):
-        self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER) #PROTOCOL_TLS_SERVER
-        self.ssl_context.load_cert_chain(self.cert_dir, self.key_dir)
-        self.start_server = websockets.serve(self.loop_logic, port=self.port, ssl=self.ssl_context)
-
     
     def run_server(self):
+        self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        self.ssl_context.load_cert_chain(self.cert_dir, self.key_dir)
+        self.start_server = websockets.serve(self.loop_logic,
+                                             port=self.port, ssl=self.ssl_context,
+                                             max_size=100000,
+                                             max_queue=4,
+                                             read_limit=2**16)
         asyncio.get_event_loop().run_until_complete(self.start_server)
         asyncio.get_event_loop().run_forever()
         
-
 
 if __name__ == "__main__":
     server = TCPServer(
@@ -82,13 +72,8 @@ if __name__ == "__main__":
         cert_dir = '../cert.pem',
         key_dir = '../privkey.pem'
     )
-    # server.run_server()
-
-    executor = ProcessPoolExecutor(max_workers=10)
-    executor.map(server.run_server())
-    # for _ in range(multiprocessing.cpu_count()):
-    #     t = Thread(target=server.run_server)
-    #     t.start()
+    loop_server = Thread(target=server.run_server(), daemon=True)
+    loop_server.start()
 
     
 
