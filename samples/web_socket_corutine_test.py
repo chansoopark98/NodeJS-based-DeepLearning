bcac7a1235a0e5a645a9a13b9229d5e9e72d1877
park-tsp <chansoo0710@gmail.com>
2022-06-29 16:46:23 +0900

    Update: coroutine test

--- samples/web_socket_corutine_test.py
+++ samples/web_socket_corutine_test.py
@@ -1,32 +1,39 @@
-from pydoc import cli
 import ssl
 import asyncio
 import websockets
 import cv2
 import numpy as np
 import base64
-from threading import Thread
-from dl_model.model_builder import SemanticModel
-import requests
-import json
 import time
-import aiotf
 import tensorflow as tf
-import grpc
-
+from aiogrpc import insecure_channel
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2_grpc
 
+# docker run -t --gpus all --rm -p 8500:8500 -v "/home/park/park/NodeJS-based-DeepLearning/samples/dl_model/saved_model:/models/test_model" -e MODEL_NAME=test_model tensorflow/serving:2.6.2-gpu
 
-async def make_prediction(model_name: str, data):
-        value = 0
-        async with aiotf.AsyncTensorflowServing('localhost:8501') as client:
-            predictions = client.predict(model_name, data)
-            # value =tf.make_ndarray(predictions.outputs['output'])
-            value = predictions
-        return value
+class RequestRestApi(object):
+    def __init__(self, host_name, model_name):
+        self.endpoint = host_name
+        self.model_name = model_name
 
-        
+    async def __aenter__(self):
+        self._channel = insecure_channel(self.endpoint)
+        self._stub = prediction_service_pb2_grpc.PredictionServiceStub(self._channel)
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self._channel.close()
+
+
+    async def predict(self, image):
+        self.request = predict_pb2.PredictRequest()
+        self.request.model_spec.name = self.model_name
+        self.request.inputs['input_1'].CopyFrom(
+        tf.make_tensor_proto(image, shape=[1, 320, 180, 3]))
+        return await self._stub.Predict(self.request, 5.0)
+
+
 class TCPServer():
     def __init__(self, hostname, port, cert_dir, key_dir):
         super().__init__()
@@ -36,13 +43,8 @@ class TCPServer():
         self.key_dir = key_dir
         self.tcp_address = 'http://localhost:8501/v1/models/test_model:predict'       
 
-        # self.channel = grpc.insecure_channel('localhost:8500', options=(('grpc.enable_http_proxy', 0),))
-        self.channel = grpc.insecure_channel('localhost:8500')
-        self.stub = prediction_service_pb2_grpc.PredictionServiceStub(self.channel)
 
-
-    async def get_data(self, data):
-        # data = data[0].split(',')
+    async def get_data(self, data, session):
         data = data.split(',')
         client_id = data[0]
         data_type = data[1]
@@ -60,74 +62,45 @@ class TCPServer():
         image = image.astype(np.float32) / 255.
         
         send_image = np.expand_dims(image, axis=0)
-        
-        # send_image = json.dumps({'instances':send_image[0:3].tolist()})
-        # output = self.client.predict('test_model', send_image)
 
-        
-        # result = requests.post(self.tcp_address, data=send_image)
+        start = time.process_time()
+        output = await session.predict(image=send_image)
 
-        # predictions = json.loads(str(result.content, 'utf-8'))['predictions']
-
-        # prediction = np.array(predictions[0])
-        # prediction = np.reshape(prediction, (320, 180, 2))
-        # prediction = np.argmax(prediction, axis=-1)
-        # prediction = prediction * 127
-
-        request = predict_pb2.PredictRequest()
-        request.model_spec.name = 'test_model'
-        # request.model_spec.signature_name = 'serving_default'
-        request.inputs['input_1'].CopyFrom(
-        tf.make_tensor_proto(send_image, shape=[1, 320, 180, 3]))
-        result = self.stub.Predict(request, 10.0)  # 10 secs timeout
-        output = result.outputs['output'].float_val
+        output = output.outputs['output'].float_val
         output = np.array(output)
         output = np.reshape(output, (320, 180, 2))
         output = np.argmax(output, axis=-1)
         output = output * 127
 
+        duration = (time.process_time() - start)
+        print("id: {0}, time: {1}".format(client_id, duration))
+
         cv2.imshow(str(client_id), output.astype(np.uint8))
         cv2.waitKey(1)
         
-        
-        # encode_image = cv2.imencode('.jpeg', output[0].numpy())
-        encode_image = cv2.imencode('.jpeg', image)
+        encode_image = cv2.imencode('.jpeg', output)
         encode_image = base64.b64encode(encode_image[1]).decode('utf-8')
         encode_image = 'data:image/jpeg;base64' + ',' + encode_image
 
-        
-        # websocket.send(encode_image)
         return client_id, encode_image
         
 
             
-        
-
     async def loop_logic(self, websocket, path):
-        while True:
-            try:
-                start = time.process_time()
-                # Wait data from client
-                # data = await asyncio.gather(websocket.recv())
-                data = await websocket.recv()
-                
-                # client_id, rcv_data = await self.get_data(data=data)
-                client_id, encode_image = await self.get_data(data=data)
-                await websocket.send(encode_image)
-                
-                
-                duration = (time.process_time() - start)
-                print("id: {0}, time: {1}".format(client_id, duration))
-                # client_id, rcv_data = self.rcv_data(data=data)
-                
+        async with RequestRestApi('localhost:8500', 'test_model') as session:
+            while True:
+                try:
+                    data = await websocket.recv()
+                    client_id, encode_image = await self.get_data(data=data, session=session)
+                    await websocket.send(encode_image)
 
-            except Exception as e:
-                print('Log : {0}'.format(e))
-                cv2.destroyWindow(str(client_id))
-                break
-                # websocket.close()
+                except Exception as e:
+                    print('Log : {0}'.format(e))
+                    cv2.destroyWindow(str(client_id))
+                    websocket.close()
+                    break
+                    
 
-    
     def run_server(self):
         self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         self.ssl_context.load_cert_chain(self.cert_dir, self.key_dir)
@@ -158,5 +131,4 @@ if __name__ == "__main__":
         cert_dir = '../cert.pem',
         key_dir = '../privkey.pem'
     )
-    # server.save_model()
     server.run_server()
